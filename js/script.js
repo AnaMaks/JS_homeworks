a575ce11e3eca5f407bcb49a3da4a823cd7e0ccf
AnaMaks <akvamarella@gmail.com>
2024-02-28 23:22:47 +0200

    hw_6 - numders and rows

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 `use strict`;
+
+// task 1
+
+let firstString = prompt("Enter 1st row:");
+let secondString = prompt("Enter 2nd row:");
+let thirdString = prompt("Enter 3rd row:");
+let result = firstString + secondString + thirdString;
+alert(result);
+
+
+// task 2
+
+const number = prompt("Please enter 5 digit number:");
+let digits = number.split("");
+let results = digits.join(" ");
+alert("Result: " + results);
