32f62a50f94be3dbce9e8b316c11d3b9e8dd4ce7
AnaMaks <akvamarella@gmail.com>
2024-03-06 21:43:36 +0200

    hw_6 - numders and rows, let changed to const

--- js/script.js
+++ js/script.js
@@ -2,16 +2,16 @@
 
 // task 1
 
-let firstString = prompt("Enter 1st row:");
-let secondString = prompt("Enter 2nd row:");
-let thirdString = prompt("Enter 3rd row:");
-let result = firstString + secondString + thirdString;
+const firstString = prompt("Enter 1st row:");
+const secondString = prompt("Enter 2nd row:");
+const thirdString = prompt("Enter 3rd row:");
+const result = firstString + secondString + thirdString;
 alert(result);
 
 
 // task 2
 
 const number = prompt("Please enter 5 digit number:");
-let digits = number.split("");
-let results = digits.join(" ");
+const digit = number.split("");
+const results = digit.join(" ");
 alert("Result: " + results);
