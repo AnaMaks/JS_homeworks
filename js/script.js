72174d56f8498431009c6eff3b257a3ad22dbff1
AnaMaks <akvamarella@gmail.com>
2024-03-11 00:23:45 +0200

    hw_10 init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 `use strict`;
+
+const a = prompt('Введіть значення А: ');
+const b = prompt('Введіть значення Б: ');
+Number(a);
+Number(b);
+const c = (Number(a)) + (Number(b));
+console.log('Результат складання А + Б = ' + c);
+const da = a - b;
+console.log('Результат віднімання А - Б = ' + da);
+const db = b - a;
+console.log('Результат віднімання Б - А = ' + db);
+const eab = a * b;
+console.log('Результат множення А * Б = ' + eab);
+const fa = a / b;
+console.log('Результат ділення А / Б = ' + fa);
+const fb = b / a;
+console.log('Результат ділення Б / А = ' + fb);
