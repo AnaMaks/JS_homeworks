ee4dc7c758839830df7f39fe9c87870c590b5888
AnaMaks <akvamarella@gmail.com>
2024-03-10 18:52:04 +0200

    hw_7 init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 `use strict`;
+
+// task #1
+
+let admin, name;
+name = "John";
+admin = name;
+alert(admin);
+
+
+// task #2
+
+const myPlanet = "Earth";
+console.log(myPlanet);
+
+const userName = prompt('Hello, enter your name:');
+console.log(userName);
+
+
+// task #3
+
+let value = '10' + 10;
+console.log(typeof value);
+console.log(value);
