36cd512bbdd6e46594a1d17e9cbe73097b993004
AnaMaks <akvamarella@gmail.com>
2024-03-10 20:27:41 +0200

    hw_8 added if else

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 `use strict`;
 
 const age = prompt('Enter your current age:');
-console.log(typeof age)
-alert(age)
+if (age !== null) {
+    // age = parseInt(age); не розумію, як саме має виглядвти ця функція або чим мржна її замінити
+    if (!isNaN(age)) {
+        const futureAge = age + 10;
+        alert("In 10 years, you\'ll be " + futureAge + " years old.");
+    } else {
+        alert("Please use a number.");
+    }
+} else {
+    alert("Oh, something went wrong.");
+}
