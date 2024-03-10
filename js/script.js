e2b4051884ef3e5f7b3181fae558e4448be8e0e7
AnaMaks <akvamarella@gmail.com>
2024-03-10 23:38:44 +0200

    hw_19 init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 `use strict`;
+
+const value = prompt('Введіть число: ');
+if (!isNaN(value)) {
+    if (value === 1) {
+        alert(value + ' pік')
+    }
+} else {
+    alert('Для продовження введіть число.')
+}
