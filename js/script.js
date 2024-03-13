6733ae70f6e10ac3e72b4f266afd92e25553d99d
AnaMaks <akvamarella@gmail.com>
2024-03-13 23:35:54 +0200

    hw_14 init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 `use strict`;
+
+const a = prompt('Введіть 1ше число: ');
+const b = prompt('Введіть 2ге число: ');
+if (!isNaN(a)) {
+    Number(a);
+    Number(b);
+    const c = (Number(a)) + (Number(b));
+    const d = a - b;
+    const f = a * b;
+    const g = a / b;
+    alert('Введені значення ' + a + ' та ' + b + ', результат: '
+        + a + ' + ' + b + ' = ' + c + ', ' +
+        + a + ' - ' + b + ' = ' + d + ', ' +
+        + a + ' * ' + b + ' = ' + f + ', ' +
+        + a + ' / ' + b + ' = ' + g + '. ')
+} else {
+    alert('Введено нечислове значення.')
+}
+
+
