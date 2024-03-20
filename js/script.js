3ebf2b8db6e2b4b5d5f94b0abfd9fea42dc493d5
AnaMaks <akvamarella@gmail.com>
2024-03-20 23:05:59 +0200

    hw_22 init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 `use strict`;
+
+// task #1
+
+let output = '';
+
+for (let i = 10; i <= 20; i++) {
+    output += i + ', ';
+}
+
+console.log(output);
+
+
+// task #2
+
+for (let i = 10; i <= 20; i++) {
+    let square = i * i;
+    console.log('Квадрат числа ' + i + ' дорівнює ' + square);
+}
+
+
+// task #3
+
+for (let i = 1; i <= 10; i++) {
+    let result = 7 * i;
+    console.log('7 * ' + i + ' = ' + result);
+}
+
+
+// task #4
+
+let sum = 0;
+
+for (let i = 1; i <= 15; i++) {
+    sum += i;
+}
+
+console.log('Сума чисел від 1 до 15: ' + sum);
